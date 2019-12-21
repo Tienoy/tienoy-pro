357cc721aa9680c786c6ceb3601f345ddda083f8
quanguanghao <quanguanghao@zongs365.com>
2019-12-21 11:18:15 +0800

    init

---
+++ src/main/java/com/tal/TienoyProApplication.java
@@ -0,0 +1,13 @@
+package com.tal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TienoyProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TienoyProApplication.class, args);
+    }
+
+}
