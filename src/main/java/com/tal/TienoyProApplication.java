3de1a32a1456f38c53fe71325f07ddffee0b69fe
quanguanghao <quanguanghao@zongs365.com>
2020-01-09 10:18:04 +0800

    add banner

--- src/main/java/com/tal/TienoyProApplication.java
+++ src/main/java/com/tal/TienoyProApplication.java
@@ -2,7 +2,12 @@ package com.tal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author tal
+ */
+@EnableScheduling
 @SpringBootApplication
 public class TienoyProApplication {
 
