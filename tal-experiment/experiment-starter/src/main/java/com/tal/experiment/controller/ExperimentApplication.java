4e3af5765d5ae15f596a81e23270a377e35d061c
TAL <tienoy520@gmail.com>
2020-06-12 16:06:07 +0800

    change project to springCloud Alibaba Project

---
+++ tal-experiment/experiment-starter/src/main/java/com/tal/experiment/controller/ExperimentApplication.java
@@ -0,0 +1,18 @@
+package com.tal.experiment.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Experiment part
+ *
+ * @Author: Tal
+ * @Date: 2020/6/12 15:27
+ */
+@SpringBootApplication
+public class ExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentApplication.class, args);
+    }
+}
